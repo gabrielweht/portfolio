2e8e765ce26f9765d03d9e4a32090ad799258f67
gabrielweht <gabrielestebanw@gmail.com>
2024-04-24 13:50:12 -0300

    Added profile and staks components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { useEffect } from 'react'
 import './App.css'
 import Navbar from './components/navbar/navbar'
+import Profile from './sections/profile/profile'
+import Stack from './sections/stack/stack'
 
 function App() {
 
+  useEffect(() => {
+    if (!window.location.hash) {
+      window.location.hash = '#profile'
+    }
+  }, [])
+
   return (
     <>
-      <div>
+        <Profile />
+        <Stack />
         <Navbar />
-      </div>
+
     </>
   )
 }
