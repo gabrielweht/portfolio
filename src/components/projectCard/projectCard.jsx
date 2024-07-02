b6e734e58632138a8ebb53c1ee67f9e0d48c5297
Gabriel Weht <gweht@maxionmontich.local>
2024-07-02 12:02:19 -0300

    Project sectios

--- src/components/projectCard/projectCard.jsx
+++ src/components/projectCard/projectCard.jsx
@@ -1,64 +1,106 @@
 /* eslint-disable react/prop-types */
-import { TbWorld } from "react-icons/tb";
+import { TfiWorld } from "react-icons/tfi";
 import { FaGithub } from "react-icons/fa";
-import './projectCard.css'
+import "./projectCard.css";
 import { useState } from "react";
 
-export function ProjectCard({children}) {
-  const [ imagesActives, setImagesActives ] = useState({
-    cabsaDesktop: true
-  })
+export function ProjectCard({ children }) {
+  const [imagesActives, setImagesActives] = useState({
+    CABSA: {
+      cabsaDesktop: true,
+      cabsaMobile: false,
+      cabsaTablet: false,
+    },
+  });
 
-  function activeImage(e){
-    e.preventDefault()
-    
+  function activeImage(e) {
+    e.preventDefault();
+
+    const target = e.target.name || e.target.id
+
+    const parent = document.getElementById(target).parentElement.id;
+
+    const lastActives = { ...imagesActives };
+
     const currentActive = {
-      ...imagesActives,
-      [e.target.name]: !imagesActives[e.target.name]
+      ...lastActives[parent],
+    };
+
+    for (let current in currentActive) {
+      if (current === target) currentActive[current] = true;
+      else currentActive[current] = false;
     }
 
     setImagesActives({
-      ...currentActive
-    })
-
-    const hola = document.getElementById(e.target.name).parentElement.id
-
-    console.log(hola)
+      ...lastActives,
+      [parent]: {
+        ...currentActive,
+      },
+    });
   }
 
   return (
     <div className="divContainerProjectCard">
+      <div className="divContainerImages">
+        <div className="divContainerImageButtons" id={children.title}>
+          {children.images.map((image, key) => {
+            return (
+              <button onClick={activeImage} id={image.title} key={key} className={
+                imagesActives[children.title][image.title]
+                  ? "buttonImage"
+                  : "buttonDisactive"
+              }>
+                <img
+                  src={image.image}
+                  alt={image.title}
+                  name={image.title}
+                  className="imageButton"
+                />
+              </button>
+            );
+          })}
+        </div>
+        {children.images.map((image, key) => {
+          return (
+            <img
+              src={image.image}
+              alt={image.title}
+              className={
+                imagesActives[children.title][image.title]
+                  ? "imgProjectCard"
+                  : "imageDisable"
+              }
+              key={key}
+            />
+          );
+        })}
+      </div>
+      <h3>{children.title}</h3>
+      <div className="divStackContainer">
         {
-          children.images.map((image, key) => {
+          children.stack.map((st, key) => {
             return(
-              <div  key={key}>
-                <div className="divContainerImageButtons" id={children.title}>
-                  <button onClick={activeImage} id={image.title}>
-                    <img src={image.image} alt={image.title} name={image.title} className="imageButton"/>
-                  </button>
-                </div>
-                <div>
-                  <img src={image.image} alt={image.title} />
-                </div>
+              <div key={key} className="divStackIconContainer">
+                <img src={st.icon} alt={st.title}/>
+                <h6>{st.title}</h6>
               </div>
             )
           })
         }
-        <header>
-          <h3>
-            {children.title}
-          </h3>
-        </header>
-        <div className="divLinks">
-          <a href={children.url} target='_blank' className='aWebSite'>
-            Website
-            <TbWorld className="iconLinks"/>
-          </a>
-          { children.github ? <a href={children.github} target='_blank' className='aGithub'>
-            Github
-            <FaGithub className="iconLinks"/>
-          </a> : undefined}
-        </div>
       </div>
-  )
+      <p>{children.description}</p>
+      <div className="divLinks">
+        <a href={children.url} target="_blank" className="aWebSite">
+          Website
+          <TfiWorld className="iconLinks" />
+        </a>
+        {children.github ? (
+          <a href={children.github} target="_blank" className="aGithub">
+            Github
+            <FaGithub className="iconLinks" />
+          </a>
+        ) : undefined}
+      </div>
+    </div>
+  );
 }
--- src/sections/projects/projects.jsx
+++ src/sections/projects/projects.jsx
@@ -5,18 +5,21 @@ import { ProjectCard } from '../../components/projectCard/projectCard'
 function Projects() {
 
     return (
-        <section className='sectionStyles section'>
-            {
-                projectList.map((project, key) => {
-                    return(
-                        <ProjectCard
-                            key={key}
-                        >
-                            {project}
-                        </ProjectCard>
-                    )
-                })
-            }
+        <section className='sectionStyles section' id='projects'>
+            <h5>Projects</h5>
+            <div className='divContainerProjects'>
+                {
+                    projectList.map((project, key) => {
+                        return(
+                            <ProjectCard
+                                key={key}
+                            >
+                                {project}
+                            </ProjectCard>
+                        )
+                    })
+                }
+            </div>
         </section>
     )
 }
