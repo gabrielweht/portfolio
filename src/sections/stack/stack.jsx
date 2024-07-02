388de1f0a9239ae56d6b63e45221dd8c9cfba0c5
Gabriel Weht <gweht@maxionmontich.local>
2024-07-02 12:03:13 -0300

    Change styles

--- src/sections/stack/stack.jsx
+++ src/sections/stack/stack.jsx
@@ -5,7 +5,7 @@ import StackListMap from '../../components/stackListMap/stackListMap.jsx'
 function Stack() {
     return (
         <section className='stackSection section' id='stacks'>
-            <h5 className='h5Skill'>{"Skills Overview"}</h5>
+            <h5>Skills Overview</h5>
             <div className='divBigStacks'>
                 {
                     stacksList.map((stack, key) => {
--- src/utils/projectsList.js
+++ src/utils/projectsList.js
@@ -2,6 +2,8 @@ import cabsaImage from '../assets/CabsaImage.webp'
 import SiCss3 from "../assets/css.webp";
 import SiTypescript from "../assets/ts.webp";
 import SiNextdotjs from "../assets/nextjs.webp";
+import dotnet from '../assets/dotnet.webp'
+import css from '../assets/css.webp'
 
 export const projectList = [
   {
@@ -10,6 +12,82 @@ export const projectList = [
       {
         title: 'cabsaDesktop',
         image: cabsaImage
+      },
+      {
+        title: 'cabsaMobile',
+        image: dotnet
+      },
+      {
+        title: 'cabsaTablet',
+        image: css
+      }
+    ],
+    description: 'dsafdasfad adfasfdsfadsfads adf adsf dsfads fdsf adsfadsfadsfafa fas fasfadfasfdsafadsf a fdffa fadsf aaaaadfasdfadsfasdfads af adsfadsf asfdsfa afads afadsf ',
+    stack: [
+      {
+        title: 'Next JS',
+        icon: SiNextdotjs
+      },
+      {
+        title: 'TypeScript',
+        icon: SiTypescript
+      },
+      {
+        title: 'CSS3',
+        icon: SiCss3
+      },
+    ],
+    url: 'https://cabsaintl.com/',
+    github: ''
+  },
+  {
+    title: 'CABSA',
+    images: [
+      {
+        title: 'cabsaDesktop',
+        image: cabsaImage
+      },
+      {
+        title: 'cabsaMobile',
+        image: dotnet
+      },
+      {
+        title: 'cabsaTablet',
+        image: css
+      }
+    ],
+    description: 'Sitio web de Financiera',
+    stack: [
+      {
+        title: 'Next JS',
+        icon: SiNextdotjs
+      },
+      {
+        title: 'TypeScript',
+        icon: SiTypescript
+      },
+      {
+        title: 'CSS3',
+        icon: SiCss3
+      },
+    ],
+    url: 'https://cabsaintl.com/',
+    github: 'https://github.com/anmedina-arg/CABSA'
+  },
+  {
+    title: 'CABSA',
+    images: [
+      {
+        title: 'cabsaDesktop',
+        image: cabsaImage
+      },
+      {
+        title: 'cabsaMobile',
+        image: dotnet
+      },
+      {
+        title: 'cabsaTablet',
+        image: css
       }
     ],
     description: 'Sitio web de Financiera',
