0f8ef223a1d57817da85c5e34af78c8195aacbd8
gabrielweht <gabrielestebanw@gmail.com>
2024-07-26 20:29:43 -0300

    responsive contact section

--- src/sections/contact/contact.jsx
+++ src/sections/contact/contact.jsx
@@ -1,11 +1,16 @@
 import { ContactForm } from '../../components/contactForm/contactForm'
+// import contactImage from '../../assets/contactImage.webp'
+import contactImage from '../../assets/contactImage.webp'
 import './contact.css'
 
 export function Contact () {
     return(
         <section className='sectionContainerContact section' id='contact'>
             <h5>CONTACT ME</h5>
-            <ContactForm />
+            <div className='divContactForm'>
+                <ContactForm /> 
+                <img src={contactImage} alt="contact image" />
+            </div>
         </section>
     )
 }
